619d5dff1ae8df22019982df0947c19ecefaae08
dsnim <dsnim35@gmail.com>
2020-07-09 16:58:15 +0900

    페이징 알고리즘 테스트
    아노테이션 추가

---
+++ src/test/java/com/spring/mvc/board/test/PagingAlgorithmTest.java
@@ -0,0 +1,12 @@
+package com.spring.mvc.board.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/mvc-config.xml"})
+public class PagingAlgorithmTest {
+	
+
+}
